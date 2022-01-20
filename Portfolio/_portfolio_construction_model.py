55860b89ee9425084905a488e168ff95ed8f0e3a
Cole Feldman <cole.d.feldman@gmail.com>
2022-01-20 13:26:14 -0800

    Added framework for the simulated trading system, and portfolio construction model.

---
+++ Portfolio/_portfolio_construction_model.py
@@ -0,0 +1,5 @@
+class PortfolioConstructionModel(object):
+    """Generates portfolio weights from trading strategy, risk model,
+    transaction model, and asset class"""
+    def __init__(self):
+        pass
